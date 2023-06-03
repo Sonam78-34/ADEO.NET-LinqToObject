277927e5b48d8d522df2b3068b9267c8c7a5699e
Sonam Kumari <126173613+Sonam78-34@users.noreply.github.com>
2023-06-03 09:44:20 +0530

    LinqToObject  file first update

---
+++ LinqToObject/LinqToObject/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqToObject
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+
+        public string job { get; set; }
+
+        public static List<Employee> GetEmployeedetail()
+        {
+            List<Employee> emplist = new List<Employee>
+            {
+                new Employee { Id = 15, name="Saurav", job ="Software Engineering"},
+                new Employee { Id = 16, name="Sona", job ="Software Engineering"},
+                new Employee { Id = 15, name="Sia", job ="Doctor"}
+            };
+            return emplist;
+
+
+        }
+    }
+}
---
+++ LinqToObject/LinqToObject/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace LinqToObject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var employeedetail = from employee in Employee.GetEmployeedetail()
+                                 orderby employee.name
+                                 select employee;
+            foreach (var item in employeedetail)
+            {
+                Console.WriteLine(item.Id +" "+item.name+" "+item.job);
+            }
+
+            Console.WriteLine(" ");
+            var employeedetail2 = from employee in Employee.GetEmployeedetail()
+                                 orderby employee.name descending
+                                 select employee;
+            foreach (var item in employeedetail2)
+            {
+                Console.WriteLine(item.Id + " " + item.name + " " + item.job);
+            }
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Elementary level function");
+
+            //ElementAt()
+            var employeethird= Employee.GetEmployeedetail().ElementAt(2);
+            Console.WriteLine(employeethird.name+" "+employeethird.job);
+
+
+
+            //ElementAtOrDefault
+            var emp1= Employee.GetEmployeedetail().ElementAt(3);
+            var emp2= Employee.GetEmployeedetail().ElementAt(0);
+            Console.WriteLine(emp2.name+" "+ emp2.Id);
+            Console.WriteLine(emp1);
+
+
+
+            //First()
+            var emp3= Employee.GetEmployeedetail().First();
+
+            Console.WriteLine("First function "+emp3.name);
+
+
+
+            //FirstOrDefault
+            var emp4= Employee.GetEmployeedetail().FirstOrDefault(j=>j.job=="Doctor");
+            Console.WriteLine("First or default "+emp4.name+emp4.job);
+
+            //last()
+            var emp5 = Employee.GetEmployeedetail().Last();
+            Console.WriteLine("The last value of emplyoee " + emp4.name + emp4.job);
+
+
+
+
+            //LastOrDefault()
+
+            var emp6 = Employee.GetEmployeedetail().LastOrDefault(j => j.name == "Sonam");
+            Console.WriteLine("LastOrDefault " + emp4.name + emp4.job);
+
+
+            //
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //LinQToObject
+            string[] namerArry = { "sonam", "Riya", "siya" };
+            IEnumerable<string> name = from studname in namerArry
+                                       where studname.Contains("sonam")
+                                       select studname;
+            foreach( string studname in name)
+            {
+                Console.WriteLine(studname);
+            }
+        }
+    }
+}
